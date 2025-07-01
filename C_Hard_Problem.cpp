cc1320dcc417f77d0439f95dd3d9423913b1e578
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2025-07-01 19:54:16 +0530

    commit

---
+++ C_Hard_Problem.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int m, a, b, c;
+        cin >> m, a, b, c;
+
+        int row1 = 0;
+        int row2 = 0;
+        int row3 = 0;
+
+        if (m < a)
+        
+
+
+
+
+            cout << row1 + row2 + row3 << endl;
+    }
+    return 0;
+}
