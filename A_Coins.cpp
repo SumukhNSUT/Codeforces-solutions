8b7fedd329ac959b8618416c13390d5fb2786f00
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2025-10-14 23:17:30 +0530

    commit

--- A_Coins.cpp
+++ A_Coins.cpp
@@ -11,13 +11,10 @@ int main() {
         long long n, k;
         cin >> n >> k;
 
-        // If n is even -> always YES (use only 2-coins)
-        // If k is odd -> always YES (we can adjust parity using one k-coin)
-        // Otherwise (n odd, k even) -> NO
-        if (n % 2 == 0 || k % 2 == 1)
-            cout << "YES\n";
-        else
+        if (n % 2 != 0 && k % 2 == 0)
             cout << "NO\n";
+        else
+            cout << "YES\n";
     }
 
     return 0;
